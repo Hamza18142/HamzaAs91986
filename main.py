36889cd02b1edea63fc2a280c5e83d911a5083c8
Hamza Gardizi <>
2021-06-04 23:44:26 +0000

    Component 1: Trial 2: Using PIL to add image, tried different padding to align widgets

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk, Image #this import to use images
 
 names=[]
 
@@ -7,24 +8,25 @@ class GUIWindow:
     def __init__(self, parent):
         
       background_color = "OldLace"
-    
-      #frame set up
-      self.quiz_frame=Frame(parent, bg=background_color, padx=100, pady=100)
+          #frame set up
+      self.quiz_frame=Frame(parent, bg=background_color)
       self.quiz_frame.grid()
-      #Create a label widget for the heading
-      self.heading_label= Label(self.quiz_frame, text="WARZONE", bg=background_color)
-      self.heading_label.grid(row=0, padx=20)
 
-      self.user_label=Label(self.quiz_frame, text="Please enter your name: ", bg=background_color)
-      self.user_label.grid(row=1, padx=20)
-
+      #open image
+      self.start_image = Image.open("start.png")
+      self.start_image = self.start_image.resize((650,550), Image.ANTIALIAS)
+      self.start_image = ImageTk.PhotoImage(self.start_image)
+      #display image in a label
+      self.image_label = Label(self.quiz_frame, image=self.start_image)
+      self.image_label.place(x=0, y=0, relwidth=1, relheight=1)
+   
       #Entry Box
       self.entry_box=Entry(self.quiz_frame)
-      self.entry_box.grid(row=2, padx=20, pady=20)
+      self.entry_box.grid(row=2, padx=250, pady=200)
 
       #Create a Button
       self.continue_button=Button (self.quiz_frame, text="Continue", bg="lime",command=self.name_collection)
-      self.continue_button.grid(row=3, padx=20, pady=20)
+      self.continue_button.grid(row=3, padx=20)
 
     def name_collection(self):
         name=self.entry_box.get()
@@ -36,7 +38,7 @@ class GUIWindow:
 #starting point of the program
 if __name__ =="__main__":
     root = Tk()#creating a window
-    root.title("WARZONE")#title of the window
+    root.title("CALL OF DUTY WARZONE")#title of the window
     open_quiz=GUIWindow(root)
     root.mainloop() #keep showing the window untill we close import
   
